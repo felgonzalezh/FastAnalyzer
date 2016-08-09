b693c3c1afd7f5c451606793ce70417b300f98d2
Felipe Gonzalez <cf.gonzalez62@uniandes.edu.co>
2016-08-09 23:04:30 +0200

    CRAB config file 2016B Data

--- MiniAODAnalyzer/python/crabConfig_DATA_SingleMuon_Run2016B_25ns_PromptReco_MiniAODv2.py
+++ MiniAODAnalyzer/python/crabConfig_DATA_SingleMuon_Run2016B_25ns_PromptReco_MiniAODv2.py
@@ -25,7 +25,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'SingleMuon_Run2016B_25ns_PromptReco_MiniAODv2'
+config.General.requestName = 'SingleMuon_Run2016B_25ns_PromptReco_MiniAODv2_total'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -39,6 +39,6 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
 config.Data.outLFNDirBase = '/store/user/cfgonzal/'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'SingleMuon_Run2016B_25ns_PromptReco_MiniAODv2'
+config.Data.outputDatasetTag = 'SingleMuon_Run2016B_25ns_PromptReco_MiniAODv2_total'
 
 config.Site.storageSite = 'T3_US_FNALLPC'
